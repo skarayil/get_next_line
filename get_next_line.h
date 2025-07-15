c58efb3d28cb274a3d9f8a2fed99c96c1cc42076
Sude Naz Karayıldırım <sudekarayildirim@gmail.com>
2025-07-15 12:14:50 +0300

    sude naz karayıldırım

--- get_next_line.h
+++ get_next_line.h
@@ -13,12 +13,12 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+# include <unistd.h>
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
 # endif
-
-# include <stdlib.h>
-# include <unistd.h>
 
 char	*get_next_line(int fd);
 size_t	ft_string_length(const char *s);
